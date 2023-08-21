74f71f929a946667f8314d15c84cfeac0988fd4c
colaturka <130190241+colaturka@users.noreply.github.com>
2023-08-20 22:37:15 -0500

    Update currency.js

--- currency.js
+++ currency.js
@@ -1,40 +1,27 @@
+import currencyapi from '@everapi/currencyapi-js';
+
+const client = new currencyapi('YOUR-API-KEY');
+
 document.getElementById('conversion-form').addEventListener('submit', function(event) {
     event.preventDefault(); // Prevent form from submitting the traditional way
 
     const amount = parseFloat(document.getElementById('value1').value);
-    const fromCurrency = document.getElementById('from').value;
-    const toCurrency = document.getElementById('to').value;
 
-    convertCurrency(fromCurrency, toCurrency, amount);
-});
-
-function convertCurrency(fromCurrency, toCurrency, amount) {
-    const API_KEY = '632da9956e4e713fb309f7a0dd0ead1b';
-    const API_URL = 'https://data.fixer.io/api/convert';
-
-    const params = new URLSearchParams({
-        access_key: API_KEY,
-        from: fromCurrency,
-        to: toCurrency,
-        amount: amount
+    client.convert({
+        value: amount
+    }).then(response => {
+        if (response.success) {
+            const convertedAmount = response.result;
+            const fromCurrency = response.query.from;
+            const toCurrency = response.query.to;
+            document.getElementById('result').textContent =
+                `${amount.toFixed(2)} ${fromCurrency} is ${convertedAmount.toFixed(6)} ${toCurrency}`;
+        } else {
+            document.getElementById('result').textContent =
+                'Currency conversion failed: ' + response.error.message;
+        }
+    }).catch(error => {
+        console.error('Error:', error);
+        document.getElementById('result').textContent = 'An error occurred.';
     });
-
-    const url = `${API_URL}?${params.toString()}`;
-
-    fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            if (data.success) {
-                const convertedAmount = data.result;
-                document.getElementById('result').textContent =
-                    `${amount.toFixed(2)} ${fromCurrency} is ${convertedAmount.toFixed(6)} ${toCurrency}`;
-            } else {
-                document.getElementById('result').textContent =
-                    'Currency conversion failed: ' + data.error.info;
-            }
-        })
-        .catch(error => {
-            console.error('Error:', error);
-            document.getElementById('result').textContent = 'An error occurred.';
-        });
-}
+});
