02cd3dee3fd1f715264e57189f84cec3920e9940
colaturka <130190241+colaturka@users.noreply.github.com>
2023-10-28 00:39:42 -0500

    Update currency.js

--- currency.js
+++ currency.js
@@ -1,4 +1,5 @@
 import CurrencyAPI from '@everapi/currencyapi-js';
+
 document.getElementById('conversion-form').addEventListener('submit', function(event) {
     event.preventDefault(); // Prevent form from submitting the traditional way
 
@@ -6,13 +7,13 @@ document.getElementById('conversion-form').addEventListener('submit', function(e
     const fromCurrency = document.getElementById('from').value;
     const toCurrency = document.getElementById('to').value;
     const currencyApi = new CurrencyAPI('cur_live_QY6JOVe8TkPSnVRQpC0vQS68znhb5ub6puf775kN');
-        currencyApi.latest({
-        base_currency: 'fromCurrency',
-        currencies: 'toCurrency'
+    
+    currencyApi.latest({
+        base_currency: fromCurrency,
+        currencies: toCurrency
     }).then(response => {
         console.log(response);
-        
+    }).catch(error => {
+        console.error(error);
     });
 });
-
-
