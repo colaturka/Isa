35cf3c56dff78eaf47b341673e913581a1c2c8af
colaturka <130190241+colaturka@users.noreply.github.com>
2024-04-05 02:17:24 -0500

    Update currency.js

--- currency.js
+++ currency.js
@@ -9,8 +9,7 @@ document.getElementById('conversion-form').addEventListener('submit', function(e
     const currencyApi = new CurrencyAPI('cur_live_QY6JOVe8TkPSnVRQpC0vQS68znhb5ub6puf775kN');
     
     currencyApi.latest({
-        base_currency: fromCurrency,
-        currencies: toCurrency
+        value:amount
     }).then(response => {
         console.log(response);
     }).catch(error => {
